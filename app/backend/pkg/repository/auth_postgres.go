961ef62208b5114b27b66f23bbc9002dcb8b1ca5
mdotv <pilipenkomaxim.v@gmail.com>
2021-11-28 12:50:16 +0300

    preparing backend for deploying

--- app/backend/pkg/repository/auth_postgres.go
+++ app/backend/pkg/repository/auth_postgres.go
@@ -17,8 +17,8 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 
 func (r *AuthPostgres) CreateUser(user todo.User) (int, error) {
 	var id int
-	query := fmt.Sprintf("INSERT INTO %s (name, username, password_hash) values ($1, $2, $3) RETURNING id", usersTable)
-	row := r.db.QueryRow(query, user.Name, user.Username, user.Password)
+	query := fmt.Sprintf("INSERT INTO %s (email, username, password_hash) values ($1, $2, $3) RETURNING id", usersTable)
+	row := r.db.QueryRow(query, user.Email, user.Username, user.Password)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
 	}
